d84c91f0352e127d8dd71f7105b1245604ec99c1
FlightSimMex <banderap03@gmail.com>
2025-04-07 19:04:54 -0400

    Add files via upload

---
+++ FinanceTrackerV2-2.1/Source/FileManager.java
@@ -0,0 +1,203 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ * Class Name: FileManager
+ * Credit: Pablo Bandera Lopez
+ * Created: 03/31/2025
+ * Modified: 04/01/2025
+ * 
+ * Description: Class deals with File I/O. Has three sets of methds:
+ * FILE PATH & DIRECTORY: Used to construct strings pointing to the files needed to read and write and create and delete files. Uses Calendar to determine the current Month and Year.
+ * PRINTING METHODS: Use PrintWriter to print out to a file based on the path given.
+ * READING METHODS: Use Scanner to read information from the input document.
+ * 
+ * Attributes:
+ * - month: String
+ * - year: String
+ * - C: Calendar
+ * 
+ * Methods:
+ * + <<constructor>>FileManager()
+ * + getCurrentMonth(): String
+ * + getCurrentYear(): String
+ * + getFilePath(String, String): String
+ * + getDirectoryPath(String): String
+ * + filePathExists(String, String): boolean
+ * + directoryExists(String): boolean
+ * + makeDir(String): void
+ * + makeFile(String): void
+ * + rmvFile(String): void
+ * + updateFile(Entries): void
+ * + readEntries(String): Entries
+ * + entryStringToEntry(String): Entry
+ * + getNumberOfEntries(String): int
+ * 
+ */
+public class FileManager {
+    
+    /*Attributes*/
+    private String month, year;
+    final private static Calendar C = Calendar.getInstance();
+
+
+    @SuppressWarnings("OverridableMethodCallInConstructor")
+    public FileManager()//Constructor
+    {   
+        
+        String m = this.getCurrentMonth();
+        String y = this.getCurrentYear();
+        if(m != null && !m.isEmpty()){this.month = m;}
+        if(y != null && !y.isEmpty()) {this.year  = y;}
+        
+    }
+
+
+    /* FILE PATH & DIRECTORY METHODS */
+    public String getCurrentMonth()
+    {
+        int monthInt = C.get(Calendar.MONTH);
+        DateFormatSymbols symbols = new DateFormatSymbols();
+        String [] months = symbols.getMonths();
+        return months[monthInt];
+    }
+
+    public String getCurrentYear()
+    {
+        int yearInt = C.get(Calendar.YEAR);
+        return String.valueOf(yearInt);
+    }
+
+    public String getFilePath(String m, String y)
+    {
+        String path = "./Files/"+y+"/"+m+".txt";
+        return path;
+    }
+
+    public String getDirectoryPath(String y)
+    {
+        String path = "./Files/"+y;
+        return path;
+    }
+    
+    public boolean filePathExists(String m, String y) {return Files.exists(Paths.get(getFilePath(m, y)));}
+    public boolean directoryExists(String y) {return Files.exists(Paths.get(getDirectoryPath(y)));}
+
+    public void makeDir(String path)
+    {
+        File f = new File(path);
+        f.mkdirs();
+    }
+
+    public void makeFile(String path)
+    {
+        File f = new File(path);
+        try{f.createNewFile();}
+        catch(IOException e){JOptionPane.showMessageDialog(null, e.toString(), "Error", JOptionPane.ERROR_MESSAGE);}
+    }
+
+    public void rmvFile(String path)
+    {
+        File f = new File(path);
+        try{f.delete();}catch(Exception e){}
+    }
+
+
+
+    /* PRINTING METHODS */
+    public void updateFile(Entries entries)//Updates the file with the current entries
+    {
+    
+        try (PrintWriter pw = new PrintWriter(new File(getFilePath(month, year)))) {
+            for(Entry e : entries.getEntries())
+            {
+                pw.println(e.toString());
+            }
+        } catch (FileNotFoundException e) 
+        {
+            JOptionPane.showMessageDialog(null, e.toString(), "Error: File Not Found", JOptionPane.ERROR_MESSAGE);
+        }
+        
+    }
+
+
+    /* READING METHODS */
+    public Entries readEntries(String path)//Loads the file into the entries
+    {
+        try (Scanner s = new Scanner(new File(path));){
+            String entryString;
+            Entry e;
+            Entries entries = new Entries();
+            
+            while(s.hasNextLine())
+            {
+                entryString = s.nextLine();
+                try{
+                    e = entryStringToEntry(entryString);
+                    entries.addEntryNoUpdate(e);
+                    
+                }catch(FileFormatException ex){}
+            }
+            s.close();
+            return entries;
+        } catch (FileNotFoundException ex) 
+        {
+            JOptionPane.showMessageDialog(null, ex.toString(), "Error: File Not Found", JOptionPane.ERROR_MESSAGE);
+        }
+        return null;
+        
+    }
+
+    public Entry entryStringToEntry(String s) throws FileFormatException
+    {
+
+        String [] tokens = s.split(",");
+        if(tokens.length < 10){throw new FileFormatException("Error Reading File");}
+        try
+        {
+            Entry e  = new Entry(Integer.parseInt(tokens[0]));
+            e.setAmount(Double.parseDouble(tokens[1]));
+            e.setType(Integer.parseInt(tokens[2]));
+            e.setCategory(Integer.parseInt(tokens[3]));
+            int i = Integer.parseInt(tokens[4]);
+            if(i > 0){e.setSubcategory(i);}
+            i = Integer.parseInt(tokens[5]);
+            if(i > 0){e.setSubcategory2(i);}
+            i = Integer.parseInt(tokens[6]);
+            if(i > 0){e.setSubcategory3(i);}
+            i = Integer.parseInt(tokens[7]);
+            if(i > 0){e.setSubcategory4(i);}
+            e.setAccount(Integer.parseInt(tokens[8]));
+            e.setComment(tokens[9]);
+            return e;
+
+        }catch(InvalidEntryException e){}
+        return null;
+    }
+
+    public int getNumberOfEntries(String path)//Returns the number of entries in the file
+    {  
+        try (Scanner s = new Scanner(new File(path));){
+            int counter = 0;
+            while(s.hasNextLine()){
+                counter ++;
+                s.nextLine();
+            }
+            s.close();
+            return counter;
+        }catch (FileNotFoundException ex){}
+        return 0;
+    }
+
+   
+}
---
+++ FinanceTrackerV2-2.1/Source/InvalidEntryException.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * Class Name: InvalidEntryException
+ * Credit: Pablo Bandera Lopez
+ * Created: 03/24/2025
+ * Modified: 
+ * 
+ * Description: Exception class for invalid entries
+ * 
+ * Attributes:
+ * 
+ * Methods:
+ * + <<constructor>>InvalidEntyrException(String)
+ * 
+ * 
+ */
+public class InvalidEntryException extends Exception {
+    public InvalidEntryException(String msg){
+        super(msg);
+    }
+    
+}
